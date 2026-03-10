8f7136d5ee32e4815f2d44d03fe5f6cdae1042bd
ChristophHotter <chr.hotter@gmail.com>
2026-03-09 21:00:14 -0400

    typo example 04-2

--- examples/04-2_two-sided-cavity_with-atom_coh-drive__cumulants.jl
+++ examples/04-2_two-sided-cavity_with-atom_coh-drive__cumulants.jl
@@ -160,6 +160,7 @@ L2_R = G_ac_drive.lindblad[2]
 
 J_add = [√(γ)*σ(α, 1, 2) for α = 1:Natoms]
 eqs2 = meanfield([a'a, σ(1, 2, 2)], H2, [L2_L, L2_R, J_add...]; order = 2)
+nothing # hide
 
 # ```math
 # \begin{align} \frac{d}{dt} \langle a^\dagger a\rangle &= -1 i g \langle a^\dagger {\sigma1}^{{12}}\rangle + \langle a^\dagger a\rangle \left( -1.0 \left( \sqrt{\kappa{L}} \right)^{2} -1.0 \left( \sqrt{\kappa{R}} \right)^{2} \right) -1 i g \langle a^\dagger {\sigma2}^{{12}}\rangle + 1 i \langle a {\sigma2}^{{21}}\rangle g -1.0 \sqrt{\kappa{L}} \langle a\rangle \mathrm{Et}\left( t \right) -1.0 \sqrt{\kappa{L}} \langle a^\dagger\rangle \mathrm{Et}\left( t \right) + 1 i g \langle a {\sigma1}^{{21}}\rangle \\ \frac{d}{dt} \langle {\sigma1}^{{22}}\rangle &= 1 i g \langle a^\dagger {\sigma1}^{{12}}\rangle -1.0 \left( \sqrt{\gamma} \right)^{2} \langle {\sigma1}^{{22}}\rangle -1 i g \langle a {\sigma1}^{{21}}\rangle \end{align}
